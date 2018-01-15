6dfed61b761448674ee4f487b4bf0d9926d28708
Wolfgang Becker <wolli.becker@gmail.com>
2018-01-15 19:11:14 +0100

    Add Users class and updateUserList event

--- server/utils/users.js
+++ server/utils/users.js
@@ -8,7 +8,13 @@ class Users {
     return user;
   }
   removeUser (id) {
-    this.users = this.users.filter((user) => user.id !== id);
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
   }
   getUser (id) {
     return this.users.find((user) => user.id === id);
